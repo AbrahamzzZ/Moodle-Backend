4a06295950e8b296ef2680fff2fd35ae4f86a1f5
AbrahamzzZ <hermanosfarfan@gmail.com>
2026-01-10 00:21:46 -0500

    Se desarrollo la logica para crear usuarios

---
+++ src/controllers/user.controller.js
@@ -0,0 +1,29 @@
+const userService = require('../services/user.service');
+
+async function createUser(req, res) {
+  try {
+    const { nombre, email, rol } = req.body;
+
+    let user = await userService.findByEmail(email);
+
+    if (!user) {
+      user = await userService.createUser({
+        nombre,
+        email,
+        rol,
+        tokenOAuth: '',
+        tokenMoodle: '',
+        activo: true
+      });
+    }
+
+    res.json(user);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Error creando usuario' });
+  }
+}
+
+module.exports = {
+  createUser
+};
