0a7804ef1843b12f4ea66177e7867f0bfc674c6d
AbrahamzzZ <hermanosfarfan@gmail.com>
2026-01-25 19:51:23 -0500

    Se desarrollo la logica para el foro

--- src/services/forum.service.js
+++ src/services/forum.service.js
@@ -1,44 +1,56 @@
 import { callMoodleApi } from '../utils/moodleClient.js';
+import { MOODLE_TOKENS } from '../config/moodleTokens.js';
 
 export async function fetchForums(courseIds = []) {
-  return callMoodleApi('mod_forum_get_forums_by_courses', {
-    courseids: courseIds,
+  return callMoodleApi({
+    token: MOODLE_TOKENS.admin,
+    wsfunction: 'mod_forum_get_forums_by_courses',
+    params: { courseids: courseIds },
   });
 }
 
 export async function fetchForumDiscussions(forumId) {
-  if (!forumId) {
-    throw new Error('forumId es requerido');
-  }
-
-  return callMoodleApi('mod_forum_get_forum_discussions', {
-    forumid: forumId,
+  return callMoodleApi({
+    token: MOODLE_TOKENS.admin,
+    wsfunction: 'mod_forum_get_forum_discussions',
+    params: { forumid: forumId },
   });
 }
 
-export async function addDiscussion(user, forumId, subject, message) {
-  const result = await callMoodleApi(
-    user.token,
-    'mod_forum_add_discussion',
-    {
-      forumid: forumId,
-      subject,
-      message,
-    }
-  );
-
-  return result;
+export async function fetchDiscussionPosts(discussionId) {
+  return callMoodleApi({
+    token: MOODLE_TOKENS.admin,
+    wsfunction: 'mod_forum_get_discussion_posts',
+    params: { discussionid: discussionId },
+  });
 }
 
-export async function replyToPost(user, postId, message) {
-  const result = await callMoodleApi(
-    user.token,
-    'mod_forum_add_discussion',
-    {
+export async function replyToPost({ postId, message, token }) {
+  return callMoodleApi({
+    token,
+    wsfunction: 'mod_forum_add_discussion_post',
+    method: 'POST',
+    params: {
+      postid: Number(postId),
+      message: message.replace(/<[^>]*>/g, ''),
+      messageformat: 0, 
+    },
+  });
+}
+
+/*export async function replyToPost({ postId, message, token }) {
+  if (!token) {
+    throw new Error('Token de Moodle requerido');
+  }
+
+  return callMoodleApi({
+    token,
+    wsfunction: 'mod_forum_add_discussion_post',
+    method: 'POST',
+    params: {
       postid: postId,
       message,
-    }
-  );
-
-  return result;
-}
+      messageformat: 1,
+    },
+  });
+}*/
